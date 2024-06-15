c4b0bd99da5dea967c1d6d4958ef044cab37fc67
Sherif <sherifnabil441@gmail.com>
2024-06-15 15:25:05 +0300

    routes update

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -7,11 +7,13 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('tickets', TicketController::class);
+    Route::apiResource('tickets', TicketController::class)->except(['update']);
     Route::put('tickets/{ticket}', [TicketController::class, 'replace']);
+    Route::patch('tickets/{ticket}', [TicketController::class, 'update']);
 
     Route::apiResource('authors', AuthorController::class);
 
-    Route::apiResource('author.tickets', AuthorTicketsController::class);
+    Route::apiResource('author.tickets', AuthorTicketsController::class)->except(['update']);
     Route::put('author/{author}/tickets/{ticket}', [AuthorTicketsController::class, 'replace']);
+    Route::patch('author/{author}/tickets/{ticket}', [AuthorTicketsController::class, 'update']);
 });
